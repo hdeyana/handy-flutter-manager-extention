56c39631b5a305d88667872ef4b5e11f6fdebf36
Handi Deyana <h.deyana@gmail.com>
2021-02-08 13:09:02 +0700

    Add Provider Generator

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,14 @@
 
 import * as vscode from 'vscode';
 import { generateGetx } from './getxgenerator';
+import { generateProvider } from './providergenerator';
 
 
 export function activate(context: vscode.ExtensionContext) {
-	let genGetX = vscode.commands.registerCommand('handy-flutter-module-generator.getx', (inUri: vscode.Uri) => generateGetx(inUri));
+	const genGetX = vscode.commands.registerCommand('handy-flutter-module-generator.getx', (inUri: vscode.Uri) => generateGetx(inUri));
+	const genProvider = vscode.commands.registerCommand('handy-flutter-module-generator.provider', (inUri: vscode.Uri) => generateProvider(inUri));
 
-	context.subscriptions.push(genGetX);
+	context.subscriptions.push(genGetX, genProvider);
 }
 
 // this method is called when your extension is deactivated
--- src/providergenerator.ts
+++ src/providergenerator.ts
@@ -8,46 +8,44 @@ import { foldersConstant } from './data/constant/folders';
 
 
 
-export async function generateGetx(inUri: vscode.Uri | undefined) {
-    let data:UserInputModel;
+export async function generateProvider(inUri: vscode.Uri | undefined) {
+    let data: UserInputModel;
     try {
         data = await getUserInput(inUri);
     } catch (error) {
         return vscode.window.showErrorMessage((error as Error).message);
     }
-  
-    await generateGetxFolders(data.uri, data.name, data.isAlreadyInit);
+
+    await generateProviderForders(data.uri, data.name, data.isAlreadyInit);
     vscode.window.showInformationMessage("Successfully created new MODULE " + camelize(data.name));
 }
 
 
 
-
-async function generateGetxFolders(uri: vscode.Uri, name: string, isAlreadyInit: boolean) {
+async function generateProviderForders(uri: vscode.Uri, name: string, isAlreadyInit: boolean) {
     const nameWithoutSpace = name.replace(" ", "");
 
     const d = uri.path + "/" + nameWithoutSpace;
     await mkdirp(d);
 
     const folders = foldersConstant;
+    folders.push('/provider');
 
-    folders.push('/controller');
 
     const options = { flag: 'wx' };
-
     for (const data of folders) {
         const newp = d + data;
 
         await mkdirp(newp);
 
-        if (data === '/controller') {
-            const controllerFileName = `${newp}/${nameWithoutSpace}controller.dart`;
-          fs.writeFileSync(controllerFileName, controllerContent(name), options);
+        if (data === '/provider') {
+            const controllerFileName = `${newp}/${nameWithoutSpace}provider.dart`;
+            fs.writeFileSync(controllerFileName, controllerContent(name), options);
         }
 
         if (data === '/screen') {
             const screenFileName = `${newp}/${nameWithoutSpace}screen.dart`;
-          fs.writeFileSync(screenFileName, screenContent(name, isAlreadyInit), options);
+            fs.writeFileSync(screenFileName, screenContent(name, isAlreadyInit), options);
         }
     }
 
@@ -57,9 +55,9 @@ async function generateGetxFolders(uri: vscode.Uri, name: string, isAlreadyInit:
 function controllerContent(name: String) {
     const camel = camelize(name);
     const content = `
-import 'package:get/get.dart';
+import 'package:flutter/foundation.dart';
 
-class ${camel}Controller extends GetxController {}`;
+class ${camel}Provider extends ChangeNotifier {}`;
 
     return content;
 }
@@ -69,29 +67,24 @@ function screenContent(name: String, isAlreadyInit: boolean) {
     const camel = camelize(name);
     const content = `
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 class ${camel}Screen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GetBuilder<${camel}Controller>(
-      builder: (${name}ontroller) {
-        return Scaffold();
-      },
-    );
+    return Scaffold();
   }
 }`;
 
     const contentWithInit = `
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:provider/provider.dart';
 
 class ${camel}Screen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GetBuilder<${camel}Controller>(
-      init: ${camel}Controller(),
-      builder: (${name}ontroller) {
+    return ChangeNotifierProvider<${camel}Provider>(
+      create: ${camel}Provider(),
+      builder: (context, widget) {
         return Scaffold();
       },
     );
@@ -100,4 +93,3 @@ class ${camel}Screen extends StatelessWidget {
 
     return isAlreadyInit ? content : contentWithInit;
 }
-
